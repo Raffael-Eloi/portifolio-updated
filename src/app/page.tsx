c33526b0a5cc9809cde4f2db30117febdf7f9005
Raffael Eloi <raffaeleloi121@gmail.com>
2026-01-09 21:29:19 -0300

    use component delegation to have an easy to read project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,10 @@
-import React from 'react';
-import MainLayout from '@/components/layout/MainLayout/MainLayout';
-import BasicInfoSection from '@/components/portfolio/BasicInfo/BasicInfoSection';
-import ExperienceSection from '@/components/portfolio/Experience/ExperienceSection';
-import EducationSection from '@/components/portfolio/Education/EducationSection';
-import ExtrasSection from '@/components/portfolio/Extras/ExtrasSection';
-import { getPortfolioData } from '@/services/portfolioService';
+import HomePage from '@/components/pages/HomePage';
 
 export const metadata = {
   title: 'Portfolio',
   description: 'My Professional Portfolio',
 };
 
-export default function Home() {
-  const data = getPortfolioData();
-
-  return (
-    <MainLayout>
-      <BasicInfoSection data={data.basicInfo} />
-      <ExperienceSection data={data.experience} />
-      <EducationSection data={data.education} />
-      <ExtrasSection data={data.extras} />
-    </MainLayout>
-  );
+export default function Page() {
+  return <HomePage />;
 }
---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import MainLayout from '@/components/layout/MainLayout/MainLayout';
+import BasicInfoSection from '@/components/portfolio/BasicInfo/BasicInfoSection';
+import ExperienceSection from '@/components/portfolio/Experience/ExperienceSection';
+import EducationSection from '@/components/portfolio/Education/EducationSection';
+import ExtrasSection from '@/components/portfolio/Extras/ExtrasSection';
+import { getPortfolioData } from '@/services/portfolioService';
+
+const HomePage: React.FC = () => {
+  const data = getPortfolioData();
+
+  return (
+    <MainLayout>
+      <BasicInfoSection data={data.basicInfo} />
+      <ExperienceSection data={data.experience} />
+      <EducationSection data={data.education} />
+      <ExtrasSection data={data.extras} />
+    </MainLayout>
+  );
+};
+
+export default HomePage;
