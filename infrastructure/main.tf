8945e6d20cd64ecf385dacf9a7911f025159dfe0
Raffael Eloi <raffaeleloi121@gmail.com>
2026-08-29 12:33:46 -0300

    fix main.tf identation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,29 +14,29 @@ resource "azurerm_static_web_app" "web_app" {
 }
 
 resource "azurerm_dns_cname_record" "www" {
-    name                = "www.raffaeleloi.dev"
-    zone_name           = "www.raffaeleloi.dev"
-    resource_group_name = azurerm_resource_group.main.name
-    ttl                 = 300
-    record              = azurerm_static_web_app.web_app.default_host_name
+  name                = "www.raffaeleloi.dev"
+  zone_name           = "www.raffaeleloi.dev"
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  record              = azurerm_static_web_app.web_app.default_host_name
 }
 
 resource "azurerm_static_web_app_custom_domain" "www" {
-    static_web_app_id = azurerm_static_web_app.web_app.id
-    domain_name       = "${azurerm_dns_cname_record.www.name}.${azurerm_dns_cname_record.www.zone_name}"
-    validation_type   = "cname-delegation"
+  static_web_app_id = azurerm_static_web_app.web_app.id
+  domain_name       = "${azurerm_dns_cname_record.www.name}.${azurerm_dns_cname_record.www.zone_name}"
+  validation_type   = "cname-delegation"
 }
 
 resource "azurerm_dns_cname_record" "apex" {
-    name                = "raffaeleloi.dev"
-    zone_name           = "raffaeleloi.dev"
-    resource_group_name = azurerm_resource_group.main.name
-    ttl                 = 300
-    record              = azurerm_static_web_app.web_app.default_host_name
+  name                = "raffaeleloi.dev"
+  zone_name           = "raffaeleloi.dev"
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  record              = azurerm_static_web_app.web_app.default_host_name
 }
 
 resource "azurerm_static_web_app_custom_domain" "apex" {
-    static_web_app_id = azurerm_static_web_app.web_app.id
-    domain_name       = "${azurerm_dns_cname_record.apex.name}.${azurerm_dns_cname_record.apex.zone_name}"
-    validation_type   = "cname-delegation"
+  static_web_app_id = azurerm_static_web_app.web_app.id
+  domain_name       = "${azurerm_dns_cname_record.apex.name}.${azurerm_dns_cname_record.apex.zone_name}"
+  validation_type   = "cname-delegation"
 }
